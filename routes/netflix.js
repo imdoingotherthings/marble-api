52c22dad90ba421dd984f01442137b8861f7c13b
Asai Andrade <asaiandrade@gmail.com>
2020-01-23 18:27:54 -0800

    using puppeteer for netflix route

--- routes/netflix.js
+++ routes/netflix.js
@@ -5,6 +5,7 @@ const fetch = require('node-fetch');
 const redis = require('redis');
 const client = redis.createClient();
 // client.auth(rtg.auth.split(":")[1]);
+const puppet = require('puppeteer');
 
 client.on('error', function(err) {
     console.log(`Redis error: ${err}`);
@@ -19,7 +20,36 @@ async function getNetflix () {
         }
     });
     const data = await response.json();
-    return data;
+    let url = (data['results'][0]['locations'][0]['url']);
+    const browser = await puppet.launch({
+        headless: true,
+        args: [
+            '--no-sandbox', 
+            "--proxy-server='direct://'",
+            '--proxy-bypass-list=*',
+            '--disable-setuid-sandbox',
+            '--ignore-certificate-errors'
+        ]
+    });
+    const page = await browser.newPage();
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36');
+    await page.goto(url);
+    let api_data = await page.evaluate(() => {
+        let api = {};
+        let desc = document.querySelector('div[class=title-info-synopsis]').innerText;
+        let year = document.querySelector('span[data-uia=item-year]').innerText;
+        let seasons = document.querySelector('span[class=test_dur_str]').innerText;
+        let tv = document.querySelector('span[class=maturity-number]').innerText;
+        let logo = document.querySelector('img[data-uia=title-logo]').src;
+        api.description = desc;
+        api.seasons = seasons;
+        api.tv_rating = tv;
+        api.logo = logo;
+        api.year = year;
+        return api;
+    });
+    await browser.close();
+    return [data, api_data];
 }
 
 router.get('/netflix', (req, res) => {
